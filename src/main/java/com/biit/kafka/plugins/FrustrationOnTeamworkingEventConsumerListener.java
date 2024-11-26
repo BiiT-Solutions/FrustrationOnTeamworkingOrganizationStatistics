15e30ead44334a96d38a11e9e92af8ae559d78f8
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-11-26 15:05:17 +0100

    Consuming own events

--- src/main/java/com/biit/kafka/plugins/FrustrationOnTeamworkingEventConsumerListener.java
+++ src/main/java/com/biit/kafka/plugins/FrustrationOnTeamworkingEventConsumerListener.java
@@ -16,6 +16,8 @@ import org.springframework.messaging.handler.annotation.Payload;
 public class FrustrationOnTeamworkingEventConsumerListener extends EventListener {
 
     public FrustrationOnTeamworkingEventConsumerListener() {
+        // Event is send from this own application.
+        super(false);
         FrustrationOnTeamworkingEventsLogger.info(this.getClass(), "Frustration On Teamworking Organization Statistics Plugin loaded!");
     }
 
